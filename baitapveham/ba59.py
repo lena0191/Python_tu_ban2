2ec821feca250e4723be106cbb3221b4a02df7fb
lena0191 <kurokawaakane9669@gmail.com>
2025-07-26 23:16:52 +0700

    Create ba59.py

---
+++ baitapveham/ba59.py
@@ -0,0 +1,13 @@
+def hv(a):
+    return a*a
+def hcn(a,b):
+    return a*b
+def htr(r):
+    return r*r*3.14
+def htg(a,b,c):
+    if(a+b>c and a+c>b and b+c>a)
+        p=(a+b+c)/2
+        return (p*(p-a)*(p-b)*(p-c))**0.5
+    else:
+        print("Loi canh tam giac")
+
